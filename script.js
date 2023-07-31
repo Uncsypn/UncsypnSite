ee2a7f4919a7aa2448f32a83dd09ac1010299934
Unc <135977564+Uncsypn@users.noreply.github.com>
2023-08-01 02:56:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Add any JavaScript code to make your website interactive
+// For example:
+/*
+const heading = document.querySelector('h1');
+heading.addEventListener('click', () => {
+    heading.style.color = 'red';
+});
+*/
