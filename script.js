3a565679a10aa0e2311650de6dab13f221574a47
Unc <135977564+Uncsypn@users.noreply.github.com>
2023-08-01 02:56:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// script.js
-
-// Add any JavaScript code to make your website interactive
-// For example:
-/*
-const heading = document.querySelector('h1');
-heading.addEventListener('click', () => {
-    heading.style.color = 'red';
-});
-*/
+// sex LMFAO
